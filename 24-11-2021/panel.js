c487dbc3463f5588c59bac462a9222d51e60e974
Elisa Andrea Abrudsky <Andrea@MacBook-Air-de-Andreita.local>
2021-11-24 21:44:30 -0300

    class login

---
+++ 24-11-2021/panel.js
@@ -0,0 +1,5 @@
+let idUserDetected = getParameterByName('id');
+console.log(idUserDetected);
+
+var id = params.get('id');
+console.log(id);
--- 24-11-2021/script.js
+++ 24-11-2021/script.js
@@ -1,16 +1,53 @@
-async function sha256(message) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder('utf-8').encode(message);
-  
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-  
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-  
-    // convert bytes to hex string
-    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-    console.log(hashHex);
-    return hashHex;
-  }
-  sha256("test"); 
+
+function checkUserLogin(){
+    let username = $('#username').val();
+    let password = $('#password').val();
+    if (username == '' || password == ''){
+        alert('Debe ingresar el usario y la contraseña para continuar');
+    }else{
+        let url = "credentials.json";
+        $.getJSON( url , function( data ) {
+           // console.log(data);
+            sha256(password).then( function(respuestaHash){
+                checkUser(data, username, respuestaHash);
+               
+            }); 
+                
+        }) 
+        .fail(function() {
+            alert( "error página en mantenimiento " );
+        })
+    }
+}
+
+function checkUser(data, username, password){
+        var user = false;
+        $.each( data.users, function( key, val ) {
+      
+            val = JSON.stringify(val);
+            val = JSON.parse(val);
+    
+            if (username === val.user){
+                user = true;
+               
+                if (password == val.password){
+                    console.log( "contraseña Correcta");
+                    $('#userId').val(val.idUser);
+                    $('#login_form').submit();
+                    return false;
+                }else{
+                    alert( "contraseña incorrecta");
+                    return false;
+                }
+            }else {
+                 console.log( "usuario no coincide");
+            }
+        })
+        
+        if (user == false){
+            console.log("Usuario no registrado");
+        }
+       
+   
+}
+
